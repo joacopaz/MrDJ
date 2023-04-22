c11e58dcf281c148008e7e92ff921dfb5d7ba623
joacopaz <joacopaz@gmail.com>
2023-04-22 18:14:37 -0300

    updated website to purge any wordpress remanent code

--- script.js
+++ script.js
@@ -12,24 +12,52 @@ const toggleMenu = (e) => {
 const submitBtn = document.querySelector("form button");
 
 if (submitBtn) {
+	const success = document.querySelector(".success");
+
 	const submitForm = async (e) => {
 		e.preventDefault();
 		const contactForm = document.querySelector("form");
 		let data = new FormData(contactForm);
 		try {
-			const response = await fetch("https://formbold.com/s/6QrvL", {
+			await fetch("https://formbold.com/s/6QrvL", {
 				method: "POST",
 				body: data,
 			});
 			alert(
 				"Tu información se ha enviado! Te contactaremos en las próximas 24 horas. Gracias por confiar en MrDJ!"
 			);
-
-			contactForm.reset();
+			contactForm.remove();
+			success.style.display = "block";
+			console.log(success.style.display);
 		} catch (error) {
 			alert("Ha ocurrido un error, por favor intenta de nuevo.");
 			console.log(error);
+			window.location.reload();
 		}
 	};
 	submitBtn.addEventListener("click", submitForm);
+	const consulta = document.querySelector("#consulta");
+	const nombre = document.querySelector("#nombre");
+	const datos = document.querySelector("#contactinfo");
+	nombre.addEventListener("keydown", (e) => {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			datos.focus();
+		}
+	});
+	datos.addEventListener("keydown", (e) => {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			consulta.focus();
+		}
+	});
+	consulta.addEventListener("keydown", (e) => {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			const confirmed = confirm(
+				"Estás a punto de enviar el formulario, ¿deseas continuar?"
+			);
+			if (confirmed) submitBtn.click();
+		}
+	});
 }
